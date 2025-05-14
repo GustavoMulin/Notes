1eae7919e4b09600d8e8161c9d4a5b92497ca22c
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-14 09:12:28 -0400

    [add]: Apresentando as Notas do usuário logado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,16 +10,9 @@ class MainController extends Controller
     public function index()
     {
         $id = session('user.id');
-        $user = User::find($id)->toArray();
         $notes = User::find($id)->notes()->get()->toArray();
 
-        echo '<pre>';
-        print_r($user);
-        print_r($notes);
-
-        die();
-
-        return view('home');
+        return view('home', ['notes' => $notes]);
     }
 
     public function newNote()
