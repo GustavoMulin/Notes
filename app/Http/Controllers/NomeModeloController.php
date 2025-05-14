694c8209ff9da60eb416bea2fce42a470d052a8f
GustavoMulin <gustavobarbosa7824@gmail.com>
2025-05-14 11:52:43 -0400

    [add]: API

---
+++ app/Http/Controllers/NomeModeloController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\NomeModelo;
+use App\Http\Requests\StoreNomeModeloRequest;
+use App\Http\Requests\UpdateNomeModeloRequest;
+
+class NomeModeloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNomeModeloRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NomeModelo $nomeModelo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNomeModeloRequest $request, NomeModelo $nomeModelo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NomeModelo $nomeModelo)
+    {
+        //
+    }
+}
